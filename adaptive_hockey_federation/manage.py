5ed9c27f78d4a19ab7be56c595fbef27bd48d890
Владислав Лисицын <curov2014@yandex.ru>
2023-10-14 02:52:06 +0300

    fix flake

--- adaptive_hockey_federation/manage.py
+++ adaptive_hockey_federation/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adaptive_hockey_federation.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'adaptive_hockey_federation.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
