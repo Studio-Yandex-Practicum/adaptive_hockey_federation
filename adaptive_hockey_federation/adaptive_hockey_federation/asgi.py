6ea7e6019b7de7cd6a7b0ff3ad86b0fca6f01354
Владислав Лисицын <curov2014@yandex.ru>
2023-10-15 20:32:56 +0300

    fix Dockerfile

--- adaptive_hockey_federation/adaptive_hockey_federation/asgi.py
+++ adaptive_hockey_federation/adaptive_hockey_federation/asgi.py
@@ -2,7 +2,9 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'adaptive_hockey_federation.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'adaptive_hockey_federation.settings'
+)
 
 application = get_asgi_application()
