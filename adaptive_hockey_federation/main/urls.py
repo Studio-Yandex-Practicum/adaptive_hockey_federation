bef3393903789c25187aeaa367207532de781b0c
Арюна Булдаева <ariun@list.ru>
2023-10-24 01:37:16 +0300

    Переподключен bootstrap через CDN

--- adaptive_hockey_federation/main/urls.py
+++ adaptive_hockey_federation/main/urls.py
@@ -6,7 +6,7 @@ app_name = 'main'
 
 
 urlpatterns = [
-    path('users', views.users, name='users'),
+    path('users/', views.users, name='users'),
     path('teams/<int:id>/', views.teams_id, name='teams_id'),
     path('teams/', views.teams, name='teams'),
     path(
