ad422b4767d8e6bf258375ad26fa8133084cd28e
Konstantin Raikhert <raikhert13@gmail.com>
2023-12-06 13:39:24 +0300

    fix to start project

--- adaptive_hockey_federation/adaptive_hockey_federation/settings.py
+++ adaptive_hockey_federation/adaptive_hockey_federation/settings.py
@@ -82,8 +82,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
