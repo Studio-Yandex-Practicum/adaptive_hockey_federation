50b47c4a1f3c6266b64dfd9e4bdc8f639e501198
ANTON KIREEV <115577582+AntiANT8406@users.noreply.github.com>
2023-12-06 13:26:53 +0300

    Создание базовых моделей (#38)
    * add base models
    
    * Add User model
    
    * Add fields to Player
    
    * Add health_card model to player. Add coach. Need further constraints and representations.
    
    * Remove all redundant models, leave only players and teams.
    
    * Fix models and make base admin view
    
    * Fix imports

--- adaptive_hockey_federation/main/models.py
+++ adaptive_hockey_federation/main/models.py
@@ -1,78 +1,267 @@
-from django.db import models
+from django.db.models import (
+    BooleanField, CASCADE, CharField, DateField, ForeignKey, ManyToManyField,
+    Model, SET_NULL, UniqueConstraint,
+)
+
+SEX_CHOICES = (
+    ('male', 'Мужской'),
+    ('female', 'Женский'),
+)
+
+NAME_FIELD_LENGTH = 256
+BASE_PERSON_FIELD_LENGTH = 256
 
 
-class Team(models.Model):
-    """Класс команды
+class BaseUniqueName(Model):
     """
-    name = models.CharField(
-        max_length=256,
+    Абстрактный класс с уникальным полем "Название" и методом отображения
+    """
+    name = CharField(
+        max_length=NAME_FIELD_LENGTH,
         verbose_name='Название',
+        unique=True,
     )
 
     class Meta:
+        ordering = ('name',)
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Location(BaseUniqueName):
+    """
+    Локация привязки команды (город, область, край)
+    или места проведения соревнования
+    """
+
+    class Meta(BaseUniqueName.Meta):
+        verbose_name = 'Территория'
+        verbose_name_plural = 'Территории'
+
+
+class Discipline(BaseUniqueName):
+    """
+    Классификация дисциплины (следж-хоккей, хоккей для незрячих, спец. хоккей)
+    """
+
+    class Meta(BaseUniqueName.Meta):
+        verbose_name = 'Дисциплина'
+        verbose_name_plural = 'Дисциплины'
+
+
+class Team(Model):
+    """
+    Модель команды.
+    """
+    name = CharField(max_length=NAME_FIELD_LENGTH, )
+    location = ForeignKey(
+        to=Location,
+        on_delete=SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='Локация команды',
+    )
+    discipline = ForeignKey(
+        to=Discipline,
+        on_delete=SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='Дисциплина команды',
+    )
+
+    class Meta:
+        default_related_name = 'teams'
         verbose_name = 'Команда'
         verbose_name_plural = 'Команды'
+        constraints = [
+            UniqueConstraint(
+                name='team_location_unique',
+                fields=['name', 'location', 'discipline'],
+            )
+        ]
 
     def __str__(self):
+        if self.location:
+            return f'{self.name} - {self.location}'
         return self.name
 
 
-class Position(models.Model):
-    """Позиция в команде
+class Position(BaseUniqueName):
     """
-    name = models.CharField(
-        max_length=256,
-        verbose_name='Название',
+    Игровая позиция игрока в команде
+    """
+
+    class Meta(BaseUniqueName.Meta):
+        verbose_name = 'Игровая позиция'
+        verbose_name_plural = 'Игровые позиции'
+
+
+class Role(BaseUniqueName):
+    """
+    Роль игрока в команде (капитан, ассистент)
+    """
+
+    class Meta(BaseUniqueName.Meta):
+        verbose_name = 'Игровая позиция'
+        verbose_name_plural = 'Игровые позиции'
+
+
+class Anamnesis(BaseUniqueName):
+    """
+    Диагноз общий
+    """
+
+    class Meta(BaseUniqueName.Meta):
+        verbose_name = 'Диагноз'
+        verbose_name_plural = 'Диагнозы'
+
+
+class RespiratoryFailure(BaseUniqueName):
+    """
+    Класс хронической дыхательной недостаточности
+    """
+
+    class Meta(BaseUniqueName.Meta):
+        verbose_name = 'Класс ХДН'
+        verbose_name_plural = 'Классы ХДН'
+
+
+class BasePerson(Model):
+    """
+    Абстрактная модель с базовой персональной информацией
+    """
+    name = CharField(
+        max_length=BASE_PERSON_FIELD_LENGTH,
+        verbose_name='Имя',
+    )
+    surname = CharField(
+        max_length=BASE_PERSON_FIELD_LENGTH,
+        verbose_name='Фамилия',
+    )
+    patronymic = CharField(
+        max_length=BASE_PERSON_FIELD_LENGTH,
+        blank=True,
+        default='',
+        verbose_name='Отчество',
     )
 
     class Meta:
-        verbose_name = 'Позиция'
-        verbose_name_plural = 'Позиции'
+        ordering = ('surname', 'name', 'patronymic')
+        abstract = True
 
     def __str__(self):
-        return self.name
+        return ' '.join([self.surname, self.name, self.patronymic])
 
 
-class BaseUserInfo(models.Model):
-    """Класс игрока
+class Player(BasePerson):
     """
-    CLS_CHOICES = [
-        ('A', 'A'),
-        ('A2', 'A2'),
-        ('B', 'B'),
-        ('B2', 'B2'),
-        ('C', 'C'),
-        ('C2', 'C2'),
-    ]
-    name = models.CharField(
-        max_length=56,
-        verbose_name='Имя',
-    )
-    surname = models.CharField(
-        max_length=56,
-    )
-    date_of_birth = models.DateTimeField(
-        verbose_name='Дата рождения',
-    )
-    team = models.ForeignKey(
-        to=Team,
-        on_delete=models.SET_NULL,
+    Модель игрока. Связь с командой "многие ко многим" на случай включения
+    игрока в сборную, помимо основного состава.
+    """
+    birth_date = DateField()
+    sex = CharField(
+        max_length=max(len(sex) for sex, _ in SEX_CHOICES),
+        choices=SEX_CHOICES,
         blank=True,
         null=True,
+        verbose_name='Пол'
+    )
+    team = ManyToManyField(
+        to=Team,
+        through='PlayerTeam',
+        verbose_name='Команда'
+    )
+
+    class Meta(BasePerson.Meta):
+        default_related_name = 'players'
+        verbose_name = 'Игрок'
+        verbose_name_plural = 'Игроки'
+        constraints = [
+            UniqueConstraint(
+                name='player_unique',
+                fields=['name', 'surname', 'patronymic', 'birth_date'],
+            )
+        ]
+
+
+class Health(Model):
+    """
+    Информация по хронической дыхательной недостаточности.
+    """
+    player = ForeignKey(
+        to=Player,
+        related_name='health',
+        on_delete=CASCADE,
+        verbose_name='Игрок',
+    )
+    respiratory_failure = ForeignKey(
+        to=RespiratoryFailure,
+        related_name='respiratory_failure_players',
+        on_delete=CASCADE,
+        verbose_name='Класс ХДН',
+    )
+    is_permanent = BooleanField(
+        default=False,
+        verbose_name='Класс ХДН подтверждён перманентно',
+    )
+    revision = CharField(
+        max_length=NAME_FIELD_LENGTH,
+        blank=True,
+        null=True,
+        verbose_name='Пересмотр класса ХДН',
+    )
+    anamnesis = ForeignKey(
+        to=Anamnesis,
+        on_delete=SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='Диагноз',
+    )
+    wheelchair = BooleanField(
+        default=False,
+        verbose_name='На коляске'
+    )
+
+    def __str__(self):
+        return (f'Медицинские показатели'
+                f' игрока {self.player.surname} {self.player.name}')
+
+
+class PlayerTeam(Model):
+    """
+    Связь "многие ко многим" игрока с командой с добавлением данных игрока
+    в этой команде.
+    """
+    player = ForeignKey(
+        to=Player,
+        related_name='player_teams',
+        on_delete=CASCADE,
+        verbose_name='Игрок',
+
+    )
+    team = ForeignKey(
+        to=Team,
+        related_name='team_players',
+        on_delete=CASCADE,
         verbose_name='Команда',
     )
-    position = models.ForeignKey(
-        to=Team,
-        on_delete=models.SET_NULL,
+    position = ForeignKey(
+        to=Position,
+        on_delete=SET_NULL,
         blank=True,
         null=True,
-        verbose_name='Позиция',
+        verbose_name='Позиция игрока',
     )
-    classification = models.CharField(
-        max_length=255,
-        choices=CLS_CHOICES,
-        default=None,
+    role = ForeignKey(
+        to=Role,
+        on_delete=SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='Статус игрока',
     )
-
-    def __str__(self):
-        return self.name
+    number = CharField(
+        max_length=NAME_FIELD_LENGTH,
+        verbose_name='Игровой номер',
+    )
--- adaptive_hockey_federation/users/models.py
+++ adaptive_hockey_federation/users/models.py
@@ -1,3 +1,68 @@
-from django.db import models  # noqa
+from django.contrib.auth.models import AbstractUser
+from django.db.models import SET_NULL, CharField, ForeignKey
+from main.models import Team
 
-# Create your models here.
+NAME_MAX_LENGTH = 256
+EMAIL_MAX_LENGTH = 256
+PHONE_MAX_LENGTH = 20
+
+ROLE_USER = 'user'
+ROLE_AGENT = 'agent'
+ROLE_MODERATOR = 'moderator'
+ROLE_ADMIN = 'admin'
+
+ROLES_CHOICES = (
+    (ROLE_USER, 'Пользователь'),
+    (ROLE_AGENT, 'Представитель команды'),
+    (ROLE_MODERATOR, 'Модератор'),
+    (ROLE_ADMIN, 'Администратор'),
+)
+
+
+class User(AbstractUser):
+    phone = CharField(
+        max_length=PHONE_MAX_LENGTH,
+    )
+    role = CharField(
+        choices=ROLES_CHOICES,
+        default=ROLE_USER,
+        max_length=max(len(role) for role, _ in ROLES_CHOICES)
+    )
+    first_name = CharField(
+        max_length=NAME_MAX_LENGTH,
+        default='',
+        null=True,
+    )
+    last_name = CharField(
+        max_length=NAME_MAX_LENGTH,
+        default='',
+        null=True,
+    )
+    team = ForeignKey(
+        to=Team,
+        on_delete=SET_NULL,
+        related_name='users',
+        verbose_name='Команда',
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('username',)
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_agent(self):
+        return self.role == ROLE_AGENT
+
+    @property
+    def is_moderator(self):
+        return self.role == ROLE_MODERATOR
+
+    @property
+    def is_admin(self):
+        return (self.role == ROLE_ADMIN) or self.is_staff
