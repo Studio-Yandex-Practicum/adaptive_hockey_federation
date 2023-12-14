5c0d8583b02b58cdef11f91ddc15dd2ee366698a
Ivan Filippov <chelyabinezzz@gmail.com>
2023-12-14 16:58:38 +0500

    Moved constants to settings.py

--- adaptive_hockey_federation/adaptive_hockey_federation/settings.py
+++ adaptive_hockey_federation/adaptive_hockey_federation/settings.py
@@ -89,5 +89,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+RESOURSES_ROOT = os.path.join(BASE_DIR, 'resourses')
+PARSER_MAIN = os.path.join(BASE_DIR, 'parser/parser.py')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
--- adaptive_hockey_federation/core/management/commands/fill-db.py
+++ adaptive_hockey_federation/core/management/commands/fill-db.py
@@ -1,16 +1,12 @@
-import os
 import subprocess
 
 from django.core.management.base import BaseCommand
 
-from adaptive_hockey_federation.settings import BASE_DIR
-
-RESOURSES_ROOT = os.path.join(BASE_DIR, 'resourses')
-PARSER_MAIN = os.path.join(BASE_DIR, 'parser/parser.py')
+from adaptive_hockey_federation.settings import PARSER_MAIN, RESOURSES_ROOT
 
 
 class Command(BaseCommand):
-    help = "Описание команды"
+    help = "Запуск парсера офисных документов."
 
     def handle(self, *args, **options):
         run_parser = subprocess.getoutput(
