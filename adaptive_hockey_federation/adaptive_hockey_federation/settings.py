11ab933ebdded863fd8a9cefe1bc0104cba01640
Алексей Сосов~ <getchdnk.gmq@yandex.ru>
2023-12-16 13:22:00 +0300

    change staticfiles dir

--- adaptive_hockey_federation/adaptive_hockey_federation/settings.py
+++ adaptive_hockey_federation/adaptive_hockey_federation/settings.py
@@ -83,7 +83,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = BASE_DIR / 'staticfiles',
 
@@ -92,7 +92,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-STATIC_ROOT = BASE_DIR / 'static'
 
 LOGIN_REDIRECT_URL = 'main:main'
 
