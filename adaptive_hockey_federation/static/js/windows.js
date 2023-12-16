9730ef20c573572bf52cbd30b79a57d56346809e
Алексей Сосов~ <getchdnk.gmq@yandex.ru>
2023-12-16 10:23:40 +0300

    add staticfiles

---
+++ adaptive_hockey_federation/static/js/windows.js
@@ -0,0 +1,14 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function openRestoreForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("RestoreForm").style.display = "block";
+
+}
+
+function closeRestoreForm() {
+  document.getElementById("RestoreForm").style.display = "none";
+
+}
