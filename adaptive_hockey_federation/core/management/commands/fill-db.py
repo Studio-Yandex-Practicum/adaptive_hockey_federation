15dd0d33187d509d331bf6cd0d4cf1d8136b5242
Петр Назаров <Pnazarov86@yandex.ru>
2023-12-21 18:00:00 +0300

    поправил засечания по factory boy

--- adaptive_hockey_federation/core/management/commands/fill-db.py
+++ adaptive_hockey_federation/core/management/commands/fill-db.py
@@ -1,17 +1,13 @@
 import subprocess
 
-from core.constants import (
-    DB_MESSAGE,
-    ROLE_ADMIN,
-    ROLE_AGENT,
-    ROLE_MODERATOR,
-    TEST_USERS_AMOUNT,
-)
+from core.constants import ROLE_ADMIN, ROLE_AGENT, ROLE_MODERATOR
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from users.factories import UserFactory
 
 ROLES = [ROLE_AGENT, ROLE_MODERATOR, ROLE_ADMIN]
+TEST_USERS_AMOUNT = 3
+DB_MESSAGE = 'Данные успешно добавлены!'
 
 
 class Command(BaseCommand):
