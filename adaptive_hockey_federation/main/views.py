19a25967c91a171a3c45d1e5512ff08ef1ce1256
Евгений Каликов <eskalikov@yandex.com>
2023-12-21 11:18:52 +0500

    Using a table template

--- adaptive_hockey_federation/main/views.py
+++ adaptive_hockey_federation/main/views.py
@@ -33,10 +33,24 @@ def teams_id(request, id):
 
 @login_required
 def teams(request):
-    list_teams = Team.objects.all
+    list_teams = Team.objects.all()
     context = {
-        'teams': list_teams,
+        'table_head': {
+            'name': 'Название команды',
+            'city': 'Город',
+            'discipline_name': 'Дисциплина',
+            'staff_team_member': 'Сотрудник команды'
+        },
+        'table_data': [],
     }
+
+    for team in list_teams:
+        context['table_data'].append({
+            'name': team.name,
+            'city': team.city,
+            'discipline_name': team.discipline_name,
+            'staff_team_member': team.staff_team_member,
+        })
     return render(request, 'main/teams.html', context)
 
 
