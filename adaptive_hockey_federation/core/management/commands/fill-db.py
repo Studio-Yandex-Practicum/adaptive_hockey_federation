c938a1e85e86c7ad8b86bf91a4a14bde9fe30e92
Mary K <wildcat.3333@yandex.ru>
2023-12-23 01:17:40 +0700

    Загрузка тестовых данных ЧАСТЬ 1

--- adaptive_hockey_federation/core/management/commands/fill-db.py
+++ adaptive_hockey_federation/core/management/commands/fill-db.py
@@ -1,8 +1,16 @@
+import fnmatch
+from os import listdir
+from os.path import isfile, join
+
 from django.core.management.base import BaseCommand
 
-from adaptive_hockey_federation.core.config.dev_settings import FIXSTURES_FILE
+from adaptive_hockey_federation.core.config.dev_settings import (
+    FIXSTURES_DIR,
+    FIXSTURES_FILE,
+)
 from adaptive_hockey_federation.parser.importing_db import (
     importing_parser_data_db,
+    importing_test_data_db,
 )
 
 
@@ -10,4 +18,16 @@ class Command(BaseCommand):
     help = "Запуск парсера офисных документов."
 
     def handle(self, *args, **options):
-        importing_parser_data_db(FIXSTURES_FILE)
+        """Загрузка распарсенных данных."""
+        # importing_parser_data_db(FIXSTURES_FILE)
+
+        """Загрузка тестовых данных из файлов формата JSON."""
+        file_names = [f for f in listdir(FIXSTURES_DIR)
+                      if isfile(join(FIXSTURES_DIR, f))]
+        for file_name in file_names:
+            if 'main_' in file_name:
+                try:
+                    importing_test_data_db(FIXSTURES_DIR, file_name)
+                except Exception as e:
+                    print(f'Ошибка вставки тестовых данных {e} -> {file_name}')
+            print('Нет файлов main_***.json для загрузки тестовых данных.')
