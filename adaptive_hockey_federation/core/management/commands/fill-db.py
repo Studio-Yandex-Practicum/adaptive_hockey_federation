cf5764536f67bbbdb5a52dd875670dc4a207aa1c
Ivan Filippov <106034945+IvanFilippov74@users.noreply.github.com>
2024-01-15 21:36:21 +0500

    Dev feature/fill test data (#95)
    * Добавлены миграции
    
    * Fix миграций
    
    * add fill-test-db/amount fixtures
    
    * доработал UserFactory
    
    * работа с фабриками
    
    * bugfix/count words for factory
    
    * добавлен  StaffTeamMemberFactory
    
    * добавлен  StaffTeamMemberFactory
    
    * refactor utils-factory
    
    * Related nosologies and diagnoses
    
    * added factory Discipline(Name, Level), Discipline
    
    * refactoring code
    
    * added factory team/palyer
    
    * refactoring PlayerFactoy
    
    * bugfix UserFactory
    
    * some fixes
    
    ---------
    
    Co-authored-by: Петр Назаров <Pnazarov86@yandex.ru>
    Co-authored-by: Mary K <wildcat.3333@yandex.ru>
    Co-authored-by: Konstantin Raikhert <raikhert13@gmail.com>

--- adaptive_hockey_federation/core/management/commands/fill-db.py
+++ adaptive_hockey_federation/core/management/commands/fill-db.py
@@ -1,8 +1,5 @@
-from core.constants import ROLE_ADMIN, ROLE_AGENT, ROLE_MODERATOR
-from django.conf import settings
+from core.config.dev_settings import FIXSTURES_DIR, FIXSTURES_FILE
 from django.core.management.base import BaseCommand
-from main.factories import CityFactory, StaffMemberFactory
-from users.factories import UserFactory
 
 from adaptive_hockey_federation.core.config.dev_settings import FILE_MODEL_MAP
 from adaptive_hockey_federation.parser.importing_db import (
@@ -11,103 +8,70 @@ from adaptive_hockey_federation.parser.importing_db import (
     importing_real_data_db,
 )
 
-ROLES = [ROLE_AGENT, ROLE_MODERATOR, ROLE_ADMIN]
-TEST_USERS_AMOUNT = 3
-DB_MESSAGE = 'Данные успешно добавлены!'
+DB_MESSAGE = "Данные успешно добавлены!"
 
 
 class Command(BaseCommand):
-    help = ("Запуск парсера офисных документов, и запись их в БД."
-            "А также создание рандомных фикстур для моделей проекта.")
+    help = "Запуск парсера офисных документов, и запись их в БД."
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '-p',
-            '--parser',
-            action='store_true',
-            help='Запуск парсера документов',
+            "-p",
+            "--parser",
+            action="store_true",
+            help="Запуск парсера документов",
         )
         parser.add_argument(
-            '-c',
-            '--city',
-            action='store_true',
-            help='Фикстуры для таблицы City'
-        )
-        parser.add_argument(
-            '-s',
-            '--staff',
-            action='store_true',
-            help='Фикстуры для таблицы StaffMember'
-        )
-        parser.add_argument(
-            '-u',
-            '--users',
-            action='store_true',
-            help='Фикстуры для таблицы Users'
-        )
-        parser.add_argument(
-            '-a',
-            '--amount',
-            type=int,
-            default=10,
-            help='Количество фикстур для создания'
-        )
-        parser.add_argument(
-            '-f',
-            '--fixtures',
-            action='store_true',
-            help='Фикстуры с реальными данными для таблиц.',
+            "-f",
+            "--fixtures",
+            action="store_true",
+            help="Фикстуры с реальными данными для таблиц.",
         )
 
-    def load_data(self):
+    def load_data(self) -> None:
         """Загрузка распарсенных данных."""
-        importing_parser_data_db(settings.FIXSTURES_FILE)
+        importing_parser_data_db(FIXSTURES_FILE)
+        return None
 
-    def load_real_data(self):
+    def load_real_data(self) -> None:
         """Загрузка реальных данных из JSON."""
         for key in FILE_MODEL_MAP.items():
-            file_name = key[0] + '.json'
-            if 'main_' in key[0]:
+            file_name = key[0] + ".json"
+            if "main_" in key[0]:
                 try:
                     clear_data_db(file_name)
                 except Exception as e:
-                    print(f'Ошибка удаления данных {e} -> '
-                          f'{file_name}')
+                    self.stdout.write(
+                        self.style.ERROR(
+                            f"Ошибка удаления данных {e} -> " f"{file_name}"
+                        )
+                    )
         items = list(FILE_MODEL_MAP.items())
         items.reverse()
         for key in items:
-            file_name = key[0] + '.json'
-            if 'main_' in key[0]:
+            file_name = key[0] + ".json"
+            if "main_" in key[0]:
                 try:
-                    importing_real_data_db(
-                        settings.FIXSTURES_DIR,
-                        file_name
+                    importing_real_data_db(FIXSTURES_DIR, file_name)
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f"Фикстуры с файла {file_name} вставлены "
+                            "в таблицы!"
+                        )
                     )
                 except Exception as e:
-                    print(f'Ошибка вставки данных {e} -> '
-                          f'{file_name}')
-
-        return 'Фикстуры с реальными данными вставлены в таблицы!'
+                    return self.stdout.write(
+                        self.style.ERROR_OUTPUT(
+                            f"Ошибка вставки данных {e} -> " f"{file_name}"
+                        )
+                    )
+        return None
 
     def handle(self, *args, **options):
         """Запись данных в БД."""
-        parser = options.get('parser')
-        fixtures = options.get('fixtures')
-        city = options.get('city', False)
-        staff_member = options.get('staff', False)
-        test_users = options.get('users', False)
-        amount = options.get('amount')
+        parser = options.get("parser")
+        fixtures = options.get("fixtures")
         if fixtures:
             self.load_real_data()
-        if city:
-            CityFactory.create_batch(amount)
-            return f'{amount} фикстур для таблицы City создано!'
-        if staff_member:
-            StaffMemberFactory.create_batch(amount)
-            return f'{amount} фикстур для таблицы StaffMemmber создано!'
         if parser:
             self.load_data()
-        if test_users:
-            for role in ROLES:
-                UserFactory.create_batch(amount, role=role)
-        self.stdout.write(self.style.SUCCESS(DB_MESSAGE))
