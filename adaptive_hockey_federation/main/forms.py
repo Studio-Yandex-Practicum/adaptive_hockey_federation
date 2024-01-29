e9550e05b8916f5f7cd67affd948c327f92a3ffe
Ivan Filippov <106034945+xxfeel@users.noreply.github.com>
2024-01-29 13:39:02 +0500

    refactoring adminPanel/Player (#125)
    * refactoring adminPanel/Player
    
    * include inline teams in fieldsets players for admin panel
    
    * added docume(Tabular) for admin-panel
    
    * update ER-Diagram
    
    * refactoring main/models.py

--- adaptive_hockey_federation/main/forms.py
+++ adaptive_hockey_federation/main/forms.py
@@ -2,6 +2,19 @@ from django import forms
 from main.models import Team
 
 
+class PlayerForm(forms.ModelForm):
+    identity_document = forms.CharField(
+        widget=forms.TextInput,
+        label='Удостоверение личности',
+        help_text='Удостоверение личности'
+    )
+    level_revision = forms.CharField(
+        widget=forms.TextInput,
+        label='Уровень ревизии',
+        help_text='Уровень ревизии',
+    )
+
+
 class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
