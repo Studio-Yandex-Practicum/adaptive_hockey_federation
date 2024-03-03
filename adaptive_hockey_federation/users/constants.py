d9f78e97a9b5eba981bb9b31b5cffa61f3740fce
Denis <panasenkodf@gmail.com>
2024-03-03 13:52:33 +0700

    Transfer permissions setting to 0003_setting_group migration

--- adaptive_hockey_federation/users/constants.py
+++ adaptive_hockey_federation/users/constants.py
@@ -1,15 +1,11 @@
 from core.constants import GROUP_ADMINS, GROUP_AGENTS, GROUP_MODERATORS
 
-GROUP_NAMES = [
-    GROUP_ADMINS,
-    GROUP_MODERATORS,
-    GROUP_AGENTS,
-]
-MODERATORS_PERMS = [
-    "change_user",
-    "change_team",
-    "change_player",
-]
-AGENTS_PERMS = [
-    "change_player",
-]
+GROUP_PERMISSION = {
+    GROUP_ADMINS: tuple(),
+    GROUP_MODERATORS: (
+        "change_user",
+        "change_team",
+        "change_player",
+    ),
+    GROUP_AGENTS: ("change_player",),
+}
--- adaptive_hockey_federation/users/migrations/0003_setting_group.py
+++ adaptive_hockey_federation/users/migrations/0003_setting_group.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.management import create_permissions
 from django.db import migrations
+from users.constants import GROUP_PERMISSION
 
 
 def set_default_groups(apps, schema_editor):
@@ -12,6 +14,25 @@ def set_default_groups(apps, schema_editor):
     )
 
 
+def set_permissions_to_groups(apps, schema_editor):
+    for app_config in apps.get_app_configs():
+        app_config.models_module = True
+        create_permissions(app_config, verbosity=0)
+        del app_config.models_module
+    Group = apps.get_model("users", "ProxyGroup")
+    Permission = apps.get_model("auth", "Permission")
+    for group in Group.objects.all():
+        if not GROUP_PERMISSION[group.name]:
+            for perm in Permission.objects.all():
+                group.permissions.add(perm)
+            continue
+        for codename in GROUP_PERMISSION[group.name]:
+            group.permissions.add(
+                Permission.objects.get(codename=codename)
+            )
+        group.save()
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("users", "0002_proxygroup_alter_user_email"),
@@ -20,5 +41,8 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunPython(
             set_default_groups,
+        ),
+        migrations.RunPython(
+            set_permissions_to_groups,
         ),
     ]
