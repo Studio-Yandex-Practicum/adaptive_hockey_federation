5df62472e30640191ecf54c6a7b9551b629507a7
Denis <panasenkodf@gmail.com>
2024-03-14 23:07:58 +0700

    Mypy

--- adaptive_hockey_federation/main/controllers/player_views.py
+++ adaptive_hockey_federation/main/controllers/player_views.py
@@ -10,8 +10,8 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 from main.forms import PlayerForm
+from main.mixins import FileUploadMixin
 from main.models import Player
-from mixins import FileUploadMixin
 
 
 class PlayersListView(
