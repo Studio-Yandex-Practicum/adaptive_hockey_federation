475e676c2d26ff9e2dc2391d0589f283438dd960
Максим Портнов <maximportnov99@yandex.ru>
2024-03-14 17:14:46 +0300

    add context if search

--- adaptive_hockey_federation/main/views.py
+++ adaptive_hockey_federation/main/views.py
@@ -14,7 +14,6 @@ class MainView(
     model = Player
     template_name = "main/players/players.html"
     context_object_name = "players"
-    paginate_by = 10
     fields = [
         "id",
         "surname",
@@ -27,22 +26,13 @@ class MainView(
     ]
 
     def get_queryset(self):
-        queryset = super().get_queryset()
+        queryset = Player.objects.all()
         search = self.request.GET.get("search")
         if search:
-            search_column = self.request.GET.get("search_column")
-            if not search_column or search_column.lower() in ["все", "all"]:
-                or_lookup = Q(surname__icontains=search) | Q(
-                    name__icontains=search
-                )
-                queryset = queryset.filter(or_lookup)
-            else:
-                search_fields = {
-                    "surname": "surname",
-                    "name": "name",
-                }
-                lookup = {f"{search_fields[search_column]}__icontains": search}
-                queryset = queryset.filter(**lookup)
+            or_lookup = Q(surname__icontains=search) | Q(
+                name__icontains=search
+            )
+            queryset = queryset.filter(or_lookup)
 
         return (
             queryset.select_related("diagnosis")
@@ -52,29 +42,35 @@ class MainView(
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        table_head = {}
-        for field in self.fields:
-            if field != "id":
-                table_head[field] = Player._meta.get_field(field).verbose_name
-        context["table_head"] = table_head
-        table_data = [
-            {
-                "surname": player.surname,
-                "name": player.name,
-                "birthday": player.birthday,
-                "gender": player.get_gender_display(),
-                "number": player.number,
-                "discipline": player.discipline if player.discipline else None,
-                "diagnosis": (
-                    player.diagnosis.name if player.diagnosis else None
-                ),  # Noqa
-                "url": reverse("main:player_id", args=[player.id]),
-                "id": player.pk,
-            }
-            for player in context["players"]
-        ]
+        search = self.request.GET.get("search")
+        if search:
+            table_head = {}
+            for field in self.fields:
+                if field != "id":
+                    table_head[field] = Player._meta.get_field(
+                        field
+                    ).verbose_name
+            context["table_head"] = table_head
+            table_data = [
+                {
+                    "surname": player.surname,
+                    "name": player.name,
+                    "birthday": player.birthday,
+                    "gender": player.get_gender_display(),
+                    "number": player.number,
+                    "discipline": (
+                        player.discipline if player.discipline else None
+                    ),
+                    "diagnosis": (
+                        player.diagnosis.name if player.diagnosis else None
+                    ),
+                    "url": reverse("main:player_id", args=[player.id]),
+                    "id": player.pk,
+                }
+                for player in context["players"]
+            ]
+            context["table_data"] = table_data
 
-        context["table_data"] = table_data
         return context
 
 
