4e36c8a891e6f4b097c13783a9006e00483a7c72
Vera <vursul@DESKTOP-NM715J4>
2024-03-18 17:10:13 +0300

    В миксин добавлено условие аутентификации

--- adaptive_hockey_federation/analytics/mixins.py
+++ adaptive_hockey_federation/analytics/mixins.py
@@ -4,6 +4,7 @@ from django.contrib.auth.mixins import AccessMixin
 class AdminRequiredMixin(AccessMixin):
 
     def dispatch(self, request, *args, **kwargs):
-        if request.user.is_moderator or request.user.is_agent:
-            return self.handle_no_permission()
+        if request.user.is_authenticated:
+            if request.user.is_moderator or request.user.is_agent:
+                return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
