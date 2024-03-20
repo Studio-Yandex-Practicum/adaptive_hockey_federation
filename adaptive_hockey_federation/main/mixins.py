5d33ae3d7dea38ffcedcc61fa210a0cbfd4cbc48
Denis Panasenko <114985447+pandenic@users.noreply.github.com>
2024-03-20 23:37:37 +0700

    Feature/download restrictions (#247)
    * Update download on player create and player edit templates. Unite player create and player edit page.
    
    * Add file validation
    
    * Add JS on file deletion on a players create/edit page
    
    * Add docs list in player id template. Refactor file uploading to mixin. Minor changes.
    
    * Update player create/edit template to meet dev.
    
    * Mypy
    
    * Add filename generation.
    
    ---------
    
    Co-authored-by: Konstantin Raikhert <raikhert13@gmail.com>

---
+++ adaptive_hockey_federation/main/mixins.py
@@ -0,0 +1,20 @@
+from core.utils import generate_file_name, is_uploaded_file_valid
+from main.models import Document
+
+
+class FileUploadMixin:
+
+    @staticmethod
+    def add_new_documents(player, new_files_names, new_files_paths):
+        for name, file in zip(new_files_names, new_files_paths):
+            if is_uploaded_file_valid(file):
+                file.name = generate_file_name(
+                    file.name, str(player.id) + "-" + name
+                )
+                Document.objects.create(player=player, file=file, name=name)
+
+    @staticmethod
+    def delete_documents(player, deleted_files_paths):
+        for doc in player.player_documemts.all():
+            if doc.file.url in deleted_files_paths:
+                doc.delete()
