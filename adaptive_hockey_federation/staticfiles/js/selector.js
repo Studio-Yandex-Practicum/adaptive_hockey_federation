bc6e0ebba394f9f48ec3b8b609d0d2dbc8a0fa49
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2024-03-21 15:44:48 +0300

    feat: add selector component

---
+++ adaptive_hockey_federation/staticfiles/js/selector.js
@@ -0,0 +1,94 @@
+class Selector {
+  constructor(availableItems, elements) {
+    this.availableItems = availableItems
+    this.selectedItems = []
+
+    this.elements = elements
+    this.availableList = elements.availableList
+    this.selectedList = elements.selectedList
+    this.deleteAllButton = elements.deleteAllButton
+    this.selectAllButton = elements.selectAllButton
+    this.searchAvailableInput = elements.searchAvailableInput
+    this.searchSelectInput = elements.searchSelectInput
+
+    this.initialize()
+  }
+
+  initialize() {
+    this.renderAvailableItems()
+    this.renderSelectedItems()
+    this.setupEventListeners()
+  }
+
+  renderAvailableItems() {
+    this.availableList.innerHTML = ''
+    this.availableItems.forEach(item => {
+      if (
+        this.searchAvailableInput.value.trim() === '' ||
+        item
+          .toLowerCase()
+          .includes(this.searchAvailableInput.value.toLowerCase().trim())
+      ) {
+        const li = document.createElement('li')
+        li.classList.add('selector-item')
+        li.textContent = item
+        li.addEventListener('click', () => this.toggleSelection(item))
+        this.availableList.appendChild(li)
+      }
+    })
+  }
+
+  renderSelectedItems() {
+    this.selectedList.innerHTML = '' // Очистим список перед рендерингом
+    this.selectedItems.forEach(item => {
+      if (
+        this.searchSelectInput.value.trim() === '' ||
+        item
+          .toLowerCase()
+          .includes(this.searchSelectInput.value.toLowerCase().trim())
+      ) {
+        const li = document.createElement('li')
+        li.classList.add('selector-item')
+        li.textContent = item
+        li.addEventListener('click', () => this.toggleSelection(item)) // Добавим обработчик для снятия выбора
+        this.selectedList.appendChild(li)
+      }
+    })
+  }
+
+  toggleSelection(item) {
+    if (this.selectedItems.includes(item)) {
+      this.selectedItems = this.selectedItems.filter(
+        selectedItem => selectedItem !== item
+      )
+    } else {
+      this.selectedItems.push(item)
+    }
+    this.renderSelectedItems()
+  }
+
+  setupEventListeners() {
+    this.deleteAllButton.addEventListener('click', () => this.removeAll())
+    this.selectAllButton.addEventListener('click', () => this.selectAll())
+    this.searchAvailableInput.addEventListener('input', () =>
+      this.renderAvailableItems()
+    ) // Добавляем слушатель ввода для поиска в доступных элементах
+    this.searchSelectInput.addEventListener('input', () =>
+      this.renderSelectedItems()
+    ) // Добавляем слушатель ввода для поиска в выбранных элементах
+  }
+
+  selectAll() {
+    this.selectedItems = [...this.availableItems]
+    this.renderSelectedItems()
+  }
+
+  removeAll() {
+    this.selectedItems = []
+    this.renderSelectedItems()
+  }
+
+  getSelectedItems() {
+    return this.selectedItems
+  }
+}
