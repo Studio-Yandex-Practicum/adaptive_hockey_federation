9deaa470899f21b6c771f5f11452173941aa27aa
Виктор Ушакевич <victorixx@yandex.ru>
2024-03-22 17:18:34 +0300

    Refactor TestUrlSmoke

--- adaptive_hockey_federation/tests/smoke_test.py
+++ adaptive_hockey_federation/tests/smoke_test.py
@@ -1,30 +1,7 @@
-import re
 from http import HTTPStatus
-from typing import Iterable
 
-from competitions.models import Competition
-from core.constants import ROLE_SUPERUSER
-from django.test import Client, TestCase
-from main.data_factories.factories import (
-    CompetitionFactory,
-    DiagnosisFactory,
-    DisciplineNameFactory,
-    DocumentFactory,
-    PlayerFactory,
-    StaffTeamMemberFactory,
-    TeamFactory,
-)
-from main.models import (
-    Diagnosis,
-    DisciplineName,
-    Document,
-    Player,
-    StaffTeamMember,
-    Team,
-)
-from tests.url_test import TEST_GROUP_NAME
-from users.factories import UserFactory
-from users.models import ProxyGroup, User
+from django.test import Client
+from tests.base import BaseTestClass, UrlTestMixin
 
 URL_MSG = (
     "{method} запрос по адресу: {msg_url} должен вернуть ответ со статусом "
@@ -58,15 +35,7 @@ ADMIN_COMPETITIONS_URLS = (
     "/admin/competitions/competition/<path:object_id>/history/",
     "/admin/competitions/competition/add/",
 )
-ADMIN_SERVICE_PAGES = (
-    "/admin/jsi18n/",
-    # TODO: Раскомментировать, когда будет понятно, как это исправить,
-    #  либо удалить, если тест этих эндпоинтов не нужен.
-    # "/admin/<url>", не очень понятно, куда это должно вести
-    # "/admin/autocomplete/", 403 даже на суперюзера.
-    # "/admin/r/<int:content_type_id>/<path:object_id>/", для обработки
-    # этого урла нужен метод get_absolute_url() в моделях.
-)
+ADMIN_SERVICE_PAGES = ("/admin/jsi18n/",)
 
 ADMIN_LOGIN = ("/admin/login/",)
 
@@ -262,101 +231,15 @@ USER_POST_URL = "/users/<int:pk>/delete/"
 UNLOAD_URLS = ("/unloads/",)
 
 
-class TestUrlsSmoke(TestCase):
-
-    superuser: User
-    user: User
-    discipline_name: DisciplineName
-    teams: Team
-    diagnosis: Diagnosis
-    player: Player
-    staff: StaffTeamMember
-    competition: Competition
-    document: Document
-
-    @classmethod
-    def setUpClass(cls) -> None:
-        super().setUpClass()
-        cls.superuser = User.objects.create_user(
-            password="super1234",
-            first_name="Супер",
-            last_name="Пользователь",
-            role=ROLE_SUPERUSER,
-            email="superuser@fake.fake",
-            is_staff=True,
-            is_superuser=True,
-        )
-        cls.user = UserFactory.create()
-        cls.discipline_name = DisciplineNameFactory.create()
-        cls.teams = TeamFactory.create()
-        cls.competition = CompetitionFactory.create()
-        cls.diagnosis = DiagnosisFactory.create()
-        cls.player = PlayerFactory.create()
-        cls.staff = StaffTeamMemberFactory.create()
-        ProxyGroup.objects.create(name=TEST_GROUP_NAME).save()
-        cls.document = DocumentFactory.create(player=cls.player)
+class TestUrlsSmoke(BaseTestClass, UrlTestMixin):
 
     def setUp(self) -> None:
         self.client = Client()
         self.super_client = Client()
         self.super_client.force_login(self.superuser)
 
-    @staticmethod
-    def _render_url(url: str, subs: tuple | str):
-        pattern = re.compile(r"<[^/]+>")
-        if not re.search(pattern, url):
-            return url
-        if isinstance(subs, str):
-            subs = (subs,)
-        for sub in subs:
-            url = re.sub(pattern, sub, url, 1)
-        if re.search(pattern, url):
-            url = re.sub(pattern, subs[-1], url)
-        return url
-
-    def url_get_test(
-        self,
-        urls: Iterable | str,
-        method: str = "get",
-        status_code: int = HTTPStatus.OK,
-        subs: tuple[str, ...] | str = "1",
-    ):
-        """Унифицированный метод для урл-тестов.
-        - urls: урл-адрес либо список или кортеж урл-адресов, подлежащих
-          тестированию;
-        - method: метод запроса (обычно "get" или "post", по умолчанию -
-          "get");
-        - status_code: ожидаемый ответ, по умолчанию - 200.
-        - subs: строка или кортеж строк подстановки для идентификаторов
-          объектов типа <int:pk> в урл-путях.
-          Например, для обработки пути:
-          "/competitions/<int:competition_id>/teams/<int:pk>/add/"
-          если передать кортеж ("1", "2"), то он преобразует путь в
-          "/competitions/1/teams/2/add/").
-          Если подстановок больше, чем элементов в кортеже "subs", то для
-          оставшихся подстановок возьмется последний элемент. Если в примере
-          выше передать строку "1" или кортеж ("1",), то урл преобразуется в
-          "/competitions/1/teams/1/add/".
-        """
-        if isinstance(urls, str):
-            urls = (urls,)
-        for url in urls:
-            url_for_message = url
-            url = self._render_url(url, subs)
-            with self.subTest(url=url):
-                response = self.super_client.__getattribute__(method.lower())(
-                    url
-                )
-                self.assertEqual(
-                    response.status_code,
-                    status_code,
-                    msg=URL_MSG.format(
-                        method=method.upper(),
-                        msg_url=url_for_message,
-                        status_code=status_code,
-                        url=url,
-                    ),
-                )
+    def get_default_client(self):
+        return self.super_client
 
     def test_main_page(self):
         self.url_get_test(INDEX_PAGE)
