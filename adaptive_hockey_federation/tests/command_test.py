a96f8d88db8e8926cc425c364fbb5d43b5a2ed8f
Nurbek878 <nurik878878@yandex.ru>
2024-03-27 00:06:07 +0400

    Переделал тесты команды использую pytest и фабрики

--- adaptive_hockey_federation/tests/command_test.py
+++ adaptive_hockey_federation/tests/command_test.py
@@ -1,91 +1,52 @@
-from typing import Any
-
-from core import constants
-from django.contrib.auth.models import Permission
-from django.test import Client, TestCase
-from main.data_factories.factories import TeamFactory
-from main.models import City, DisciplineName, Team
-from tests.fixture_user import (
-    test_email,
-    test_lastname,
-    test_name,
-    test_password,
-    test_role_user,
-)
-from users.models import ProxyGroup, User
-
-TEST_GROUP_NAME = "no_permission_group"
+import pytest
+from main.data_factories.factories import DisciplineNameFactory, TeamFactory
+from main.models import Team
+from users.factories import UserFactory
 
 
-class TestCommand(TestCase):
-    user: User | Any = None
-    team: Team | Any = None
+@pytest.fixture
+def create_discipline_name():
+    return DisciplineNameFactory()
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        """Создает необходимые сущности (объекты моделей БД)."""
-        super().setUpClass()
-        ProxyGroup.objects.create(name=TEST_GROUP_NAME).save()
-        constants.GROUPS_BY_ROLE[test_role_user] = TEST_GROUP_NAME
-        cls.user = User.objects.create_user(
-            password=test_password,
-            first_name="cls_" + test_name,
-            last_name="cls_" + test_lastname,
-            role=test_role_user,
-            email="cls_" + test_email,
-        )
-        cls.team = Team.objects.create(
-            name="cls_Test Team",
-            city=City.objects.create(name="cls_Test City"),
-            discipline_name=DisciplineName.objects.create(
-                name="cls_Test DisciplineName"
-            ),
-            curator=cls.user,
-        )
 
-    def setUp(self):
-        self.client = Client()
-        self.permissions = {
-            "view_team": Permission.objects.get(codename="view_team")
-        }
+@pytest.fixture
+def create_user():
+    return UserFactory()
 
-    # TODO: Добавить проверку пермишенов после их полной реализации
-    def test_create_team(self):
-        """Тест - создание команды."""
-        new_team = TeamFactory.create()
-        self.assertIsNotNone(new_team, "Ошибка при создании команды")
 
-    # TODO: Добавить проверку пермишенов после их полной реализации
-    def test_read_team(self):
-        """Тест - чтения команды."""
-        new_team = TeamFactory.create()
-        try:
-            retrieved_team = Team.objects.get(id=new_team.id)
-            self.assertEqual(new_team.name, retrieved_team.name)
-        except Team.DoesNotExist:
-            self.fail("Ошибка при чтении команды: тестовая команда не найдена")
+@pytest.fixture
+def create_team(create_discipline_name, create_user):
+    return TeamFactory(discipline_name=create_discipline_name,
+                       curator=create_user)
 
-    # TODO: Добавить проверку пермишенов после их полной реализации
-    def test_update_team(self):
-        """Тест - обновления команды."""
-        new_team = TeamFactory.create()
-        new_name = "Updated Team"
-        new_team.name = new_name
-        try:
-            new_team.save()
-            updated_team = Team.objects.get(id=new_team.id)
-            self.assertEqual(updated_team.name, new_name)
-        except Exception:
-            self.fail("Ошибка при обновлении команды")
 
-    # TODO: Добавить проверку пермишенов после их полной реализации
-    def test_delete_team(self):
-        """Тест - удаления команды."""
-        new_team = TeamFactory.create()
-        new_team_id = new_team.id
-        try:
-            new_team.delete()
-            with self.assertRaises(Team.DoesNotExist):
-                Team.objects.get(id=new_team_id)
-        except Exception:
-            self.fail("Ошибка при удалении команды")
+@pytest.mark.django_db
+def test_create_team(create_team):
+    assert create_team is not None, "Не удалось создать команду"
+
+
+@pytest.mark.django_db
+def test_read_team(create_team):
+    # TODO: Добавить проверку пермишенов для чтения команды
+    team_id = create_team.id
+    read_team = Team.objects.filter(pk=team_id).first()
+    assert read_team is not None, "Не удалось считать информацию о команде"
+
+
+@pytest.mark.django_db
+def test_update_team(create_team):
+    # TODO: Добавить проверку пермишенов для обновления команды
+    new_team_name = "Updated Team"
+    create_team.name = new_team_name
+    create_team.save()
+    updated_team = Team.objects.get(pk=create_team.pk)
+    assert updated_team.name == new_team_name, "Не удалось обновить команду"
+
+
+@pytest.mark.django_db
+def test_delete_team(create_team):
+    # TODO: Добавить проверку пермишенов для удаления команды
+    team_id = create_team.id
+    create_team.delete()
+    deleted_team = Team.objects.filter(pk=team_id).first()
+    assert deleted_team is None, "Не удалось удалить команду"
