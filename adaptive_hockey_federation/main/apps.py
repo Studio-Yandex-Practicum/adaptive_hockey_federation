789072adee7c7c9207687552f7f0a6186e94c6ba
Konstantin Raikhert <raikhert13@gmail.com>
2024-03-29 12:25:05 +0300

    111

--- adaptive_hockey_federation/main/apps.py
+++ adaptive_hockey_federation/main/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class MainConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main'
-    verbose_name = 'Главное'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "main"
+    verbose_name = "Главное"
+    qwe = "1111"
