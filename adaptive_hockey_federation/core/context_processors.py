146b8058fdbe63c6df3adbd52a1139631d7b0925
Nikita Salikov <nik5275622@mail.ru>
2024-04-18 15:33:43 +0300

    Добавил docstring для своих изменений

--- adaptive_hockey_federation/core/context_processors.py
+++ adaptive_hockey_federation/core/context_processors.py
@@ -2,4 +2,7 @@ from main.schemas.main_schema import no_search_pages
 
 
 def search_form_context(request):
+    """Отвечает за правильное отображение поиска на страницах.
+    Если нужно где-то убрать поле поиска, нужно внести изменения в список
+    main/schemas/main_schema.py"""
     return {"no_search_pages": no_search_pages}
