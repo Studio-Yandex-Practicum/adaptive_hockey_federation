fa1894af4cd51f4f7dfa3986d80ec4b33baa7ef5
ssb000ss <ssb000ss@gmail.com>
2024-05-14 13:14:33 +0500

    Add verbose names

--- adaptive_hockey_federation/competitions/apps.py
+++ adaptive_hockey_federation/competitions/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CompetitionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'competitions'
+    verbose_name = 'Соревнования'
