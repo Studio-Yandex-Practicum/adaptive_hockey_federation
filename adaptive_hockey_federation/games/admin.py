5adf97e823cd4c0a4783de864b6ad18e7c13c23d
Milkyaway13 <129880524+Milkyaway13@users.noreply.github.com>
2024-05-23 17:06:45 +0300

    Add games app (#393)
    * Add games app
    
    * add new jpg in ER
    
    * Add func in admin
    
    * Fix mypy error
    
    ---------
    
    Co-authored-by: Konstantin Raikhert <raikhert13@gmail.com>

---
+++ adaptive_hockey_federation/games/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from games.models import Game, GameTeam
+
+
+class GameTeamInline(admin.StackedInline):
+    model = GameTeam
+    extra = 2
+
+
+class GameAdmin(admin.ModelAdmin):
+    """Админка для модели Выгрузки."""
+    inlines = [GameTeamInline]
+    list_display = ("name", "video_link", "get_teams")
+    search_fields = ("name",)
+    ordering = ["name"]
+
+    def get_teams(self, obj):
+        return ", ".join([team.name for team in obj.teams.all()])
+    get_teams.short_description = 'Teams'  # type: ignore[attr-defined]
+
+
+admin.site.register(Game, GameAdmin)
