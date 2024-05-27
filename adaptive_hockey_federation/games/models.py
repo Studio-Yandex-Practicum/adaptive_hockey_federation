5a06d4ff827d9baa8b03ddaa9adbfa51914d7df7
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-05-27 14:32:28 +0300

    feat: overhaul games' models, fix admin panel accordingly

--- adaptive_hockey_federation/games/models.py
+++ adaptive_hockey_federation/games/models.py
@@ -1,56 +1,124 @@
-from core.constants import NAME_MAX_LENGTH
+from competitions.models import Competition
+from core.constants import (
+    MAX_PLAYER_NUMBER,
+    MIN_PLAYER_NUMBER,
+    NAME_MAX_LENGTH,
+)
+from core.validators import validate_game_date
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.db.models.functions import Now
 from django.utils.translation import gettext_lazy as _
 from main.models import Team
 
 
 class Game(models.Model):
-    """
-    Модель игры.
-    """
-    name = models.CharField(_("Название игры"), max_length=NAME_MAX_LENGTH)
-    video_link = models.URLField(_("Ссылка на видео"))
-    teams = models.ManyToManyField(  # type:ignore
-        Team,
-        through='GameTeam',
-        through_fields=('game', 'team'),
-        verbose_name=_("Команды"),
-        help_text=_("Команды участвующие в игре"),
+    """Модель игры."""
+
+    name = models.CharField(
+        verbose_name=_("Название игры"), max_length=NAME_MAX_LENGTH
     )
+    date = models.DateTimeField(
+        verbose_name=_("Дата игры"),
+        validators=[validate_game_date],  # type: ignore[list-item]
+    )
+    competition = models.ForeignKey(
+        Competition,
+        on_delete=models.CASCADE,
+        verbose_name=_("Соревнование"),
+        related_name="games",
+    )
+    game_teams = models.ManyToManyField(
+        Team,
+        verbose_name=_("Команды"),
+        related_name="teams",
+    )
+    video_link = models.URLField(verbose_name=_("Ссылка на видео"), blank=True)
 
     class Meta:
         verbose_name = "Игра"
         verbose_name_plural = "Игры"
-        ordering = ['name']
+        ordering = ["name"]
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(date__lte=Now()),
+                name="game_date_must_not_be_in_the_future",
+            ),
+        ]
 
     def __str__(self):
         return self.name
 
 
 class GameTeam(models.Model):
-    """
-    Промежуточная модель, связь с командой "Многие ко многим".
-    """
-    game = models.ForeignKey(
-        Game,
-        on_delete=models.CASCADE,
-        related_name='game_teams',
-        verbose_name=_("Игра"),
-        help_text=_("Игра, в которой участвует команда"),
+    """Модель команды, участвующей в игре."""
+
+    name = models.CharField(
+        verbose_name=_("Название команды"),
+        help_text=_("Название команды"),
+        max_length=NAME_MAX_LENGTH,
     )
-    team = models.ForeignKey(
+    discipline_name = models.CharField(
+        verbose_name=_("Дисциплина"),
+        help_text=_("Дисциплина"),
+        max_length=NAME_MAX_LENGTH,
+    )
+    game_players = models.ManyToManyField(
         Team,
-        on_delete=models.CASCADE,
-        related_name='game_teams',
-        verbose_name=_("Команда"),
-        help_text=_("Команда, участвующая в игре"),
+        verbose_name=_("Игроки"),
+        help_text=_("Игроки, участвующие в игре"),
+        related_name="players",
     )
-    is_away = models.BooleanField(_("Выездная команда"), default=False)
 
     class Meta:
-        unique_together = ('game', 'team')
         verbose_name = "Команда в игре"
-        verbose_name_plural = "Команды в играх"
+        verbose_name_plural = "Команды в игре"
+        ordering = ["name"]
 
     def __str__(self):
-        return f"{self.game} - {self.team}"
+        return self.name
+
+
+class GamePlayer(models.Model):
+    """Модель игрока, участвующего в игре."""
+
+    name = models.CharField(
+        verbose_name=_("Игрок"), max_length=NAME_MAX_LENGTH
+    )
+    number = models.PositiveSmallIntegerField(
+        verbose_name=_("Номер игрока"),
+        help_text=_("Номер игрока"),
+        validators=[
+            MinValueValidator(
+                MIN_PLAYER_NUMBER,
+                _("Номер игрока должен быть больше или равен нулю"),
+            ),
+            MaxValueValidator(
+                MAX_PLAYER_NUMBER,
+                _("Номер игрока должен быть меньше или равен 99"),
+            ),
+        ],
+    )
+    game_team = models.CharField(
+        verbose_name=_("Команда"),
+        help_text=_("Команда"),
+        max_length=NAME_MAX_LENGTH,
+    )
+
+    class Meta:
+        unique_together = ("name", "number")
+        verbose_name = "Игрок в игре"
+        verbose_name_plural = "Игроки в игре"
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(number__gte=MIN_PLAYER_NUMBER),
+                name="player_number_must_be_positive",
+            ),
+            models.CheckConstraint(
+                check=models.Q(number__lte=MAX_PLAYER_NUMBER),
+                name=f"player_number_be_{MAX_PLAYER_NUMBER}_or_less",
+            ),
+        ]
+
+    def __str__(self):
+        return self.name
