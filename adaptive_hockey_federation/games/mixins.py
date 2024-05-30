bf88aeaaa25b9ff5b899573299424c668e6de91c
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-05-30 12:55:40 +0300

    feat: add mixin for DRY purposes

---
+++ adaptive_hockey_federation/games/mixins.py
@@ -0,0 +1,38 @@
+from typing import Any
+
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+)
+from django.urls import reverse_lazy
+
+from games.constants import Literals, TITLE_MAPPING
+from games.models import Game
+
+
+class GameCreateUpdateMixin(LoginRequiredMixin, PermissionRequiredMixin):
+    """Миксин для представлений создания и редактирования игры."""
+
+    model = Game
+    success_url = reverse_lazy("games:games")
+    template_name = "main/games/game_create_edit.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        """Метод для получения словаря context в шаблоне страницы."""
+        context = super(GameCreateUpdateMixin, self).get_context_data(**kwargs)
+        return dict(
+            **context,
+            page_title=TITLE_MAPPING[type(self).__name__],
+            help_text_role=Literals.CHOSEN_TEAMS,
+        )
+
+    # def get_success_url(self) -> str:
+    #     """
+    #     Метод для получения URL-адреса для перенаправления по
+    #     успешному заполнению формы.
+    #     """
+    #     return reverse(
+    #         "games:game_id", kwargs={"pk": self.object.pk}
+    #     )
+    # TODO: раскомментировать, когда появится контроллер для просмотра
+    #  игры
