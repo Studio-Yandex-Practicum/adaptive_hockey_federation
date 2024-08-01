3ba8639b7e9e00dad430d419773c37b307a12590
Паутов Александр <sh2d000w@gmail.com>
2024-08-01 13:09:59 +0300

    fix: Add type hinting. fix: Refactor code.

--- adaptive_hockey_federation/core/ydisk_utils/utils.py
+++ adaptive_hockey_federation/core/ydisk_utils/utils.py
@@ -23,30 +23,29 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
 
-def download_file(video_link, media_data_path):
+def download_file(video_link: str, media_data_path: str) -> str | None:
     """Скачать файл с Yandex Disk."""
+    error_message: str | None = None
     client = yadisk.Client(token=settings.YANDEX_DISK_OAUTH_TOKEN)
 
     with client:
         if not client.check_token():
-            error = "Токен Yandex Disk недействителен."
-            logger.exception(error)
-            return error
+            error_message = "Токен Yandex Disk недействителен."
+            logger.exception(error_message)
+            return error_message
 
         try:
             client.download_by_link(video_link, media_data_path)
         except PathNotFoundError:
-            error = f"Файл {video_link} не найден на Yandex Disk."
-            logger.exception(error)
-            return error
+            error_message = f"Файл {video_link} не найден на Yandex Disk."
+            logger.exception(error_message)
         except ForbiddenError:
-            error = "Не хватает прав, чтобы выполнить запрос."
-            logger.exception(error)
-            return error
+            error_message = "Не хватает прав, чтобы выполнить запрос."
+            logger.exception(error_message)
         except ResourceIsLockedError:
-            error = f"Файл {video_link} заблокирован другой операцией."
-            logger.exception(error)
-            return error
+            error_message = f"Файл {video_link} заблокирован другой операцией."
+            logger.exception(error_message)
         except Exception as error:
             logger.exception("Error")
-            return error
+            return str(error)
+        return error_message
