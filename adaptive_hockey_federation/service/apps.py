09dd2dd46039e525fcf7208964197bf9e6ff3649
Паутов Александр <sh2d000w@gmail.com>
2024-08-11 20:21:27 +0300

    fix: Correct docstring in apps.py.

--- adaptive_hockey_federation/service/apps.py
+++ adaptive_hockey_federation/service/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 
 class ServiceConfig(AppConfig):
-    """Конфигурация приложения Video API."""
+    """Конфигурация приложения Service."""
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "service"
