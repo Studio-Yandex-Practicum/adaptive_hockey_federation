9e2fff817234e8e5b028938d99578060756d85b8
Паутов Александр <sh2d000w@gmail.com>
2024-08-25 15:38:48 +0300

    fix: Correct adding team to GameTeam.
    Delete mock-code for request to DS server.

--- adaptive_hockey_federation/games/signals.py
+++ adaptive_hockey_federation/games/signals.py
@@ -1,12 +1,10 @@
-from pathlib import Path
-import json
-import random
-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 from games.models import Game, GamePlayer, GameTeam
+
+# from games.views import send_game_video_to_process
 from main.models import Player, Team
-from video_api.tasks import get_player_video_frames
 
 
 @receiver(post_save, sender=Game, dispatch_uid="unique_signal")
@@ -17,12 +15,10 @@ def create_game_teams(sender, instance, created, **kwargs):
     Для последующего использования сигнала при обновлении объекта Game
     реализовано удаление старых GameTeam, которые ссылались на этот Game.
     """
-    teams = instance.competition.teams.all()
     queryset_teams = list(
-        map(lambda x: Team.objects.get(id=x.id), teams),
+        map(lambda x: Team.objects.get(id=x), instance.teams),
     )
     GameTeam.objects.filter(game=instance).delete()
-    queryset_teams = random.sample(queryset_teams, 2)
     for team in queryset_teams:
         game_team = GameTeam(
             id=team.id,
@@ -35,30 +31,14 @@ def create_game_teams(sender, instance, created, **kwargs):
         )
         game_team.save()
 
-    # Моковая реализация запуска воркера по отправке запроса на
-    # обработку видео и сохранение фреймов игрока в бд
-    # TODO для реального запроса к дс-ам данные запрашиваются в бд,
-    # сериализуются и оптравляются в воркер
     # TODO необходимо пересмотреть логику создания объекта игры,
     # конкретно изменение
     # номеров игроков. Оно должно происходить до того
     # как объект игры попадёт в бд.
-    if created:
-        # пример тестовых данных для запроса берется из сервиса дс-ов
-        test_data = (
-            Path(__file__).resolve().parent.parent.parent
-            / "a_hockey-main/app/src/test/test_query_process.json"
-        )
-
-        with open(test_data, "r") as file:
-            data_from_json = json.load(file)
-            request_data = {"json": data_from_json}
-
-        get_player_video_frames.apply_async(
-            kwargs={"data": request_data},
-            queue="process_queue",
-            priority=255,
-        )
+    # TODO отправлять видео на распознавание до изменения номеров игроков
+    # нелогично.
+    # if created and instance.video_link:
+    #     send_game_video_to_process(instance.id)
 
 
 @receiver(post_save, sender=GameTeam, dispatch_uid="unique_signal")
