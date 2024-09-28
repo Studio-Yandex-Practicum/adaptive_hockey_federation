df1e6684843df6d723c3741178b2e3efba4f5279
Олег Говоровский <oleq.gov@ya.ru>
2024-09-28 13:36:42 +0500

    pre-realease8

--- adaptive_hockey_federation/manage.py
+++ adaptive_hockey_federation/manage.py
@@ -7,7 +7,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.config.prod_settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          "core.config.prod_settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
