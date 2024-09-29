9a7f6fe451c1c6ff4ca9f2d89328cded66a63c62
Олег Говоровский <oleq.gov@ya.ru>
2024-09-30 00:10:40 +0500

    test build 5

--- adaptive_hockey_federation/core/config/base_settings.py
+++ adaptive_hockey_federation/core/config/base_settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from re import DEBUG
 
 import environ
 from django.contrib.messages import constants as messages
--- adaptive_hockey_federation/core/config/dev_settings.py
+++ adaptive_hockey_federation/core/config/dev_settings.py
@@ -1,4 +1,4 @@
-from . base_settings import *
+from .base_settings import *
 
 ROOT_DIR = BASE_DIR.parent
 
