a26618bf85bd9230a1b53a667f3793720ffe32e0
Konstantin <webn1nja@users.noreply.github.com>
2024-10-07 14:45:43 +0300

    games button removed; game signals disabled (#555)
    * games button removed; game signals disabled
    
    * games signals and button deleted
    
    * returned the signals file so that there would be no conflict during the merge
    
    * fix ruff
    
    ---------
    
    Co-authored-by: Konstantin Raikhert <raikhert13@gmail.com>

--- adaptive_hockey_federation/games/apps.py
+++ adaptive_hockey_federation/games/apps.py
@@ -7,9 +7,3 @@ class GamesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "games"
     verbose_name = "Игры"
-
-    def ready(self) -> None:
-        """Импортирование сигналов для приложения."""
-        import games.signals  # noqa
-
-        return super().ready()
