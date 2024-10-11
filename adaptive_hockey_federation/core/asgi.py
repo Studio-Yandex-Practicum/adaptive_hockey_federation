82f6fab7c68ef400a820132d339583418d2bfedf
Konstantin Raikhert <raikhert13@gmail.com>
2024-10-11 11:15:08 +0300

    fix deploy

--- adaptive_hockey_federation/core/asgi.py
+++ adaptive_hockey_federation/core/asgi.py
@@ -4,7 +4,7 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    "core.config.dev_settings",
+    "core.config.prod_settings",
 )
 
 application = get_asgi_application()
